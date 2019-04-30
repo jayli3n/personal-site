6be22ff57ac1a9aeab521cdd7dab8f73c2156073
Jay Li <jay.li23697@gmail.com>
2019-04-30 15:25:19 +0800

    added variables

---
+++ src/variables.js
@@ -0,0 +1,18 @@
+export const color-primary = '#22282c';
+export const color-primary-dark-1 = '#0B0D0F';
+
+export const color-grey-1 = '#333';
+export const color-grey-2 = '#777';
+export const color-grey-3 = '#999';
+export const color-grey-4 = '#aaa';
+export const color-grey-5 = '#ccc';
+export const color-grey-6 = '#f0eeee';
+export const color-grey-7 = '#f4f2f2';
+export const color-grey-8 = '#faf9f9';
+
+
+export const maxWidth = 1005;
+
+export const smartphoneSize = 500;
+export const tabletSize = 650;
+export const pcSize = 900;
