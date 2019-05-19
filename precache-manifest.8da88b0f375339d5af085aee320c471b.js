beee8d16ce6b2ce5924404abb2755394c64108c9
Jay Li <jayli3n@gmail.com>
2019-05-19 16:46:45 +0800

    Updates

---
+++ precache-manifest.8da88b0f375339d5af085aee320c471b.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476be256e31a56ea38ae5c5559318a2c",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "e170dca14b0a28f52d95",
+    "url": "/personal-site/static/js/2.d1d53271.chunk.js"
+  },
+  {
+    "revision": "c4bfbece511738abbfed",
+    "url": "/personal-site/static/js/main.ae6ac16f.chunk.js"
+  },
+  {
+    "revision": "d33cc63b615d83e32684",
+    "url": "/personal-site/static/js/runtime~main.039145d2.js"
+  },
+  {
+    "revision": "28f8cf103c721d64a23774852c1149fd",
+    "url": "/personal-site/static/media/Jay_Li_Resume_2019.28f8cf10.pdf"
+  },
+  {
+    "revision": "135f44b0ed55d7029d056523c56d3c14",
+    "url": "/personal-site/static/media/StratumNo1 Bold.135f44b0.ttf"
+  },
+  {
+    "revision": "304df3d1a0d511d1ae5e2b6e5d9aa07a",
+    "url": "/personal-site/static/media/StratumNo1 Heavy.304df3d1.ttf"
+  },
+  {
+    "revision": "e4bdc1ebeb221092a0b0c46b0ce5011d",
+    "url": "/personal-site/static/media/StratumNo1 Light.e4bdc1eb.ttf"
+  },
+  {
+    "revision": "06a6ac1709970857f419dc78aca05353",
+    "url": "/personal-site/static/media/StratumNo1 Medium.06a6ac17.ttf"
+  },
+  {
+    "revision": "09bccbfcb6b8a6425e90786b7bac436c",
+    "url": "/personal-site/static/media/StratumNo1 Regular.09bccbfc.ttf"
+  },
+  {
+    "revision": "90223d37816bacaadc1d7659f6859cf7",
+    "url": "/personal-site/static/media/StratumNo1 Thin.90223d37.ttf"
+  },
+  {
+    "revision": "bafc4be389087c1c2c6b8e3c2231f09a",
+    "url": "/personal-site/static/media/android.bafc4be3.svg"
+  },
+  {
+    "revision": "975b7882216d98f6aa9ee3c1abc599c1",
+    "url": "/personal-site/static/media/avatar.975b7882.jpg"
+  },
+  {
+    "revision": "fe35993fd308247892f4dd71e6a70788",
+    "url": "/personal-site/static/media/book.fe35993f.png"
+  },
+  {
+    "revision": "b1b360f517849f9eb939165d18738714",
+    "url": "/personal-site/static/media/bootstrap.b1b360f5.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/personal-site/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "d70b9fda7c0377715f81c982ca7632fc",
+    "url": "/personal-site/static/media/facial-recognition-web-app_snapshot.d70b9fda.jpg"
+  },
+  {
+    "revision": "df8bf7ad749ccc8ae99516952a8f2878",
+    "url": "/personal-site/static/media/firebase.df8bf7ad.svg"
+  },
+  {
+    "revision": "b73214e3ebad909ef6891be949c39947",
+    "url": "/personal-site/static/media/git.b73214e3.svg"
+  },
+  {
+    "revision": "a513777759f9f86839af0bc6ec157910",
+    "url": "/personal-site/static/media/github.a5137777.svg"
+  },
+  {
+    "revision": "808f00f58842d193db1cc314266c11dc",
+    "url": "/personal-site/static/media/gmail.808f00f5.svg"
+  },
+  {
+    "revision": "6c613299984ecaf6de7d8061787cb7ef",
+    "url": "/personal-site/static/media/google_translate.6c613299.svg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/personal-site/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/personal-site/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "c14b5b30c52af654ebfb69258535e27f",
+    "url": "/personal-site/static/media/jayli3n-site.c14b5b30.jpg"
+  },
+  {
+    "revision": "6f58459b59dd6de3de3fe34c0bc52212",
+    "url": "/personal-site/static/media/jquery.6f58459b.svg"
+  },
+  {
+    "revision": "8ed633989375e0e8a9a361d29453c7da",
+    "url": "/personal-site/static/media/linear-gradient_snapshot.8ed63398.jpg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/personal-site/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "7792a29f1418737d6cfb84d90b4ebbbb",
+    "url": "/personal-site/static/media/lit.7792a29f.png"
+  },
+  {
+    "revision": "342a34f29a4081d9d0715c960567770d",
+    "url": "/personal-site/static/media/menu.342a34f2.svg"
+  },
+  {
+    "revision": "3b60a3a8a19ec8fcef60f51d428c247b",
+    "url": "/personal-site/static/media/microsoftaccess.3b60a3a8.svg"
+  },
+  {
+    "revision": "efbc5cbb2b9d23d33c443f04bf017df6",
+    "url": "/personal-site/static/media/natours-tour-startup_snapshot.efbc5cbb.jpg"
+  },
+  {
+    "revision": "02ab3328e1f4bd15ea15c2560388f82b",
+    "url": "/personal-site/static/media/nexter-real-estate_snapshot.02ab3328.jpg"
+  },
+  {
+    "revision": "4edea5d49bd09fd7b5eda2e56d6ab18b",
+    "url": "/personal-site/static/media/node-dot-js.4edea5d4.svg"
+  },
+  {
+    "revision": "dae8cc21cf7c400dacbeb56f5c625968",
+    "url": "/personal-site/static/media/pin.dae8cc21.svg"
+  },
+  {
+    "revision": "34cbdb156ea57b239962b2909e0a6d89",
+    "url": "/personal-site/static/media/postgresql.34cbdb15.svg"
+  },
+  {
+    "revision": "3fe2815380f515b3fba0247ec0cb70f2",
+    "url": "/personal-site/static/media/racket.3fe28153.png"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/personal-site/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "280e514ca216947a428b9fbe0a9e183b",
+    "url": "/personal-site/static/media/redux.280e514c.svg"
+  },
+  {
+    "revision": "643d0db4b0852bdf249ac130d63d1fd6",
+    "url": "/personal-site/static/media/sass.643d0db4.svg"
+  },
+  {
+    "revision": "751ece1e5b1242f5d376a27a97f68f58",
+    "url": "/personal-site/static/media/shopify.751ece1e.svg"
+  },
+  {
+    "revision": "54ab7cbd59688828d3bdfe0b76393a27",
+    "url": "/personal-site/static/media/starwars-api_snapshot.54ab7cbd.jpg"
+  },
+  {
+    "revision": "9927c9397725000278a9010674ee0f49",
+    "url": "/personal-site/static/media/trillo-hotel-booking_snapshot.9927c939.jpg"
+  },
+  {
+    "revision": "4597a5895907d536fd8b4422f879beeb",
+    "url": "/personal-site/static/media/tutor.4597a589.png"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/personal-site/static/media/twitter.be066898.svg"
+  },
+  {
+    "revision": "2bf210e0a838f2a6dbf61fe31457d6b1",
+    "url": "/personal-site/static/media/ultrave.2bf210e0.jpg"
+  },
+  {
+    "revision": "717dedb8dabaa51ddea6c5eed0ff8793",
+    "url": "/personal-site/static/media/woodplc.717dedb8.jpg"
+  },
+  {
+    "revision": "d3cbe74bf8d0f3f4d0cf68811cecf24a",
+    "url": "/personal-site/static/media/workout.d3cbe74b.png"
+  }
+]);
